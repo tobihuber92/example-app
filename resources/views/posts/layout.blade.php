99de5086134f428319638abfd6896d0c568c21de
Tobias Huber <tobias.huber.oppenau@outlook.de>
2021-11-19 14:27:24 +0100

    Crud Template in Nav-Bar eingebaut

--- resources/views/posts/layout.blade.php
+++ resources/views/posts/layout.blade.php
@@ -1,20 +1,29 @@
 <html>
 <head>
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-</x-app-layout>
-
-    <title>Laravel 8 CRUD Application - laravelcode.com</title>
+    <title>Crud</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
-<div class="container">
-    @yield('content')
-</div>
-   
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Crud') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="container">
+                        @yield('content')
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
+
+
 </body>
 </html>
